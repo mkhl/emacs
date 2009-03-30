ed352a7651de9cf6bcbba0873a26f8ee872db6f9
Martin Kuehl <martin.kuehl@gmail.com>
2009-03-30 23:24:34 +0200

    • Add mouse.el which configures mousing behaviour.

---
+++ emacs.d/mkhl/mouse.el
@@ -0,0 +1,8 @@
+
+(when window-system
+  ;; Mouse
+  (setq mouse-yank-at-point t
+        mouse-wheel-follow-mouse t
+        mouse-wheel-progressive-speed nil
+        mouse-avoidance-mode 'exile
+        mac-emulate-three-button-mouse nil))
