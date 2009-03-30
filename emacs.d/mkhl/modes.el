079b27b2733d638b8eb6a9a2f842b1e974281694
Martin Kuehl <martin.kuehl@gmail.com>
2009-03-30 23:25:05 +0200

    • Add modes.el which… doesn’t do anything yet.

---
+++ emacs.d/mkhl/modes.el
@@ -0,0 +1,7 @@
+
+;; "Generic" major modes
+;; (require 'generic-x)
+
+;;  '(c-default-style (quote ((java-mode . "java") (awk-mode . "awk") (other . "linux"))))
+;;  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+;;  '(gud-tooltip-mode t)
