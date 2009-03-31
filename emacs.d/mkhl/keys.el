d868838a7b013ab31b36481e3a08a694113dd2b1
Martin Kuehl <martin.kuehl@gmail.com>
2009-03-31 15:32:20 +0200

    • Load browse-kill-ring when available.

--- emacs.d/mkhl/keys.el
+++ emacs.d/mkhl/keys.el
@@ -6,6 +6,14 @@
 ;; (require 'bs)
 ;; (global-set-key [(control x) (control b)] #'bs-show)
 
+;; Kill ring
+(when (require 'browse-kill-ring nil 'noerror)
+  (setq browse-kill-ring-highlight-current-entry t
+        browse-kill-ring-highlight-inserted-item t
+        browse-kill-ring-display-duplicates nil
+        browse-kill-ring-no-duplicates t)
+  (browse-kill-ring-default-keybindings))
+
 ;; Undo/Redo
 (require 'redo)
 (global-set-key [(control _)] #'undo)
