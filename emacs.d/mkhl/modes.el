bb8d3b359718c69a4daf9e9ac85b13520091316c
Martin Kuehl <martin.kuehl@gmail.com>
2009-03-31 22:48:51 +0200

    Enable more ``generic'' modes.
     • Load `generic-x' on startup.

--- emacs.d/mkhl/modes.el
+++ emacs.d/mkhl/modes.el
@@ -1,4 +1,6 @@
 
+(require 'generic-x)
+
 (autoload 'indent-tabs-maybe "indent-tabs-maybe"
   "Set `indent-tabs-mode' according to buffer contents."
   t)
@@ -18,9 +20,6 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook '(lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-
-;; "Generic" major modes
-;; (require 'generic-x)
 
 ;;  '(c-default-style (quote ((java-mode . "java") (awk-mode . "awk") (other . "linux"))))
 ;;  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
