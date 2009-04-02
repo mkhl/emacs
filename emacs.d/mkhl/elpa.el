b8c59b7e1643915b1ca22500872c70e4b97d812e
Martin Kuehl <martin.kuehl@gmail.com>
2009-04-03 01:05:34 +0200

    Make yasnippet indent when point is in indentation.
    Wrap `yas/expand' in around-advice; when point is in indentation, lookup
    default action from current local and global keymaps, otherwise just run
    `yas/expand'.

--- emacs.d/mkhl/elpa.el
+++ emacs.d/mkhl/elpa.el
@@ -1,3 +1,11 @@
 
 (require 'package)
 (package-initialize)
+
+(eval-after-load "yasnippet-bundle"
+  '(defadvice yas/expand (around mkhl-yas/expand-or-indent)
+     (let ((in-indentation? (looking-back (rx bol (0+ blank)))))
+       (if in-indentation?
+           (funcall (or (lookup-key (current-local-map) yas/trigger-key)
+                        (lookup-key (current-global-map) yas/trigger-key)))
+         ad-do-it))))
