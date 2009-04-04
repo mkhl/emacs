829892b32d36300a5a7528a3050e6f5e40a57d31
Martin Kuehl <martin.kuehl@gmail.com>
2009-04-04 23:13:36 +0200

    Use quote instead of sharp-quote for key bindings.

--- emacs.d/mkhl/keys.el
+++ emacs.d/mkhl/keys.el
@@ -1,22 +1,22 @@
 
 ;; Don't use Alt-x, use Ctrl-x-m
-(global-set-key [(control x) (control m)] #'execute-extended-command)
+(global-set-key [(control x) (control m)] 'execute-extended-command)
 
 ;; Buffer switching
 (require 'ibuffer)
-(global-set-key [(control x) (control b)] #'ibuffer)
+(global-set-key [(control x) (control b)] 'ibuffer)
 ;; (require 'bs)
-;; (global-set-key [(control x) (control b)] #'bs-show)
+;; (global-set-key [(control x) (control b)] 'bs-show)
 
 ;; Directory editing
-(global-set-key [(control x) (control d)] #'dired)
+(global-set-key [(control x) (control d)] 'dired)
 
 ;; Undo/Redo
 ;; (require 'redo)
-;; (global-set-key [(control _)] #'undo)
-;; (global-set-key [(meta _)]    #'redo)
-;; (global-set-key [(control z)] #'undo)
-;; (global-set-key [(meta z)]    #'redo)
+;; (global-set-key [(control _)] 'undo)
+;; (global-set-key [(meta _)]    'redo)
+;; (global-set-key [(control z)] 'undo)
+;; (global-set-key [(meta z)]    'redo)
 
 ;; Window switching
 ;; (require 'windmove)
