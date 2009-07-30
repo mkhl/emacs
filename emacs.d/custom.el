4fa709529a5022170ef3ffdce9c882af07f51d32
Martin Kuehl <martin.kuehl@gmail.com>
2009-07-30 11:41:44 +0200

    Set up nice default colours and fonts.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -11,4 +11,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:stipple nil :background "grey96" :foreground "grey20" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Anonymous_Pro"))))
+ '(cursor ((t (:background "black"))))
+ '(hl-line ((t (:background "grey87")))))
