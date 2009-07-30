f9ce9e2844ab724981e1f4d5551e2b5ea4c61148
Martin Kuehl <martin.kuehl@gmail.com>
2009-07-30 11:01:37 +0200

    Disable scroll-in-place. Doesn't work well on OS X.

--- emacs.d/mkhl/mouse.el
+++ emacs.d/mkhl/mouse.el
@@ -1,9 +1,9 @@
 
 (when (featurep 'mouse)
   ;; Mouse
+  ;; (require 'scroll-in-place nil 'noerror)
   (setq mouse-yank-at-point t
         mouse-wheel-follow-mouse t
         mouse-wheel-progressive-speed nil
         mouse-avoidance-mode 'exile
-        mac-emulate-three-button-mouse nil)
-  (require 'scroll-in-place nil 'noerror))
+        mac-emulate-three-button-mouse nil))
