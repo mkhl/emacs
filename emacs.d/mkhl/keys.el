1eca3f75d326ea3a57cd721b2af614c7370f7a87
Martin Kuehl <martin.kuehl@gmail.com>
2009-08-08 22:35:17 +0200

    Add autoindentation advice.

--- emacs.d/mkhl/keys.el
+++ emacs.d/mkhl/keys.el
@@ -40,3 +40,31 @@
   (if (featurep 'aquamacs)
       (define-key osx-key-mode-map (kbd "A-<return>") #'next-line-and-indent)
     (global-set-key (kbd "M-<return>") #'next-line-and-indent)))
+
+;; Indent yanked text
+;; - This doesn't work because the defadvice bodies are only executed
+;;   when the advised function was called, i.e. outside of the lexical
+;;   environment defined with let and labels.
+;; - Workaround: Code duplication. Whoopdeedoo.
+;; (let ((indenting-modes '(emacs-lisp-mode
+;;                          scheme-mode
+;;                          lisp-mode
+;;                          c-mode
+;;                          c++-mode
+;;                          objc-mode
+;;                          latex-mode
+;;                          plain-tex-mode)))
+;;   (labels ((indent-yanked-region ()
+;;              (when (member major-mode indenting-modes)
+;;                (let ((mark-even-if-inactive t))
+;;                  (indent-region (region-beginning) (region-end) nil)))))
+;;     (defadvice yank (after mkhl/indent-region activate)
+;;       (funcall #'indent-yanked-region))
+;;     (defadvice yank-pop (after indent-region activate)
+;;       (funcall #'indent-yanked-region))))
+
+;; Kill and join
+(defadvice kill-line (around mkhl/kill-or-join-line activate)
+  (if (and (eolp) (not (bolp)))
+      (delete-indentation t)
+    ad-do-it))
