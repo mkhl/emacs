dddbe03f6dffa0f7f82494b6247dd59078871584
Martin Kuehl <martin.kuehl@gmail.com>
2009-08-08 22:38:12 +0200

    Fix yas/expand advice.

--- emacs.d/mkhl/elpa.el
+++ emacs.d/mkhl/elpa.el
@@ -3,7 +3,7 @@
 (package-initialize)
 
 (eval-after-load "yasnippet-bundle"
-  '(defadvice yas/expand (around mkhl-yas/expand-or-indent)
+  '(defadvice yas/expand (around mkhl/yas/expand-or-indent activate)
      (let ((in-indentation? (looking-back (rx bol (0+ blank)))))
        (if in-indentation?
            (funcall (or (lookup-key (current-local-map) yas/trigger-key)
