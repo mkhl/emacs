d7762841469fea27810172c5aad037e27ddbd6d4
Martin Kuehl <martin.kuehl@gmail.com>
2009-08-11 00:32:01 +0200

    Load a default color-theme.

--- emacs.d/mkhl/local.el
+++ emacs.d/mkhl/local.el
@@ -6,3 +6,8 @@
         browse-kill-ring-display-duplicates nil
         browse-kill-ring-no-duplicates t)
   (browse-kill-ring-default-keybindings))
+
+;; Color theme
+(when (require 'color-theme nil 'noerror)
+  (color-theme-initialize)
+  (color-theme-scintilla))
