5048d0de0a31d3b1c3a360053180a685a356e246
Martin Kuehl <martin.kuehl@gmail.com>
2009-08-10 20:40:12 +0200

    Reorganize and comment modes.el.
    Add a largish scheme section loading gambit.el and scheme-complete.el.

--- emacs.d/mkhl/modes.el
+++ emacs.d/mkhl/modes.el
@@ -1,24 +1,23 @@
 
 (require 'generic-x)
 
+(require 'dired-x)
+(setq-default dired-omit-mode t)
+
+;;; Indentation
 (autoload 'indent-tabs-maybe "indent-tabs-maybe"
   "Set `indent-tabs-mode' according to buffer contents." t)
 (add-hook 'find-file-hook 'indent-tabs-maybe 'append)
 
-(require 'dired-x)
-(setq-default dired-omit-mode t)
-
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
-;; (add-hook 'emacs-lisp-mode-hook '(lambda () (paredit-mode +1)))
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-;; (add-hook 'lisp-interaction-mode-hook '(lambda () (paredit-mode +1)))
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-
-(setq inferior-lisp-program "/opt/ccl/scripts/ccl64 -K utf-8")
-
+;;; Perl
 (defalias 'perl-mode 'cperl-mode)
 
+;;; Emacs-Lisp
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+
+;;; Common Lisp
+(setq inferior-lisp-program "/opt/ccl/scripts/ccl64 -K utf-8")
 (require 'slime-autoloads nil 'noerror)
 (eval-after-load "slime"
   '(progn
@@ -26,6 +25,34 @@
 ;;      (setq slime-complete-symbol*-fancy t)
 ;;      (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)))
      (slime-setup '(slime-fancy))))
+
+;;; Scheme
+(setq scheme-program-name "gsi -:d-")
+;; Gambit.el
+(when (locate-library "gambit")
+  (autoload 'gambit-inferior-mode "gambit" "Hook Gambit mode into cmuscheme.")
+  (autoload 'gambit-mode "gambit" "Hook Gambit mode into scheme.")
+  (add-hook 'inferior-scheme-mode-hook #'gambit-inferior-mode)
+  (add-hook 'scheme-mode-hook #'gambit-mode))
+;; Scheme-Complete.el
+(labels ((autoload-scm-cmpl (func) (autoload func "scheme-complete" nil t)))
+  (let* ((functions '(scheme-complete-or-indent
+                      scheme-smart-indent-function
+                      scheme-get-current-symbol-info)))
+    (map 'nil 'autoload-scm-cmpl functions)))
+(eval-after-load 'scheme
+  '(define-key scheme-mode-map (kbd "<tab>") 'scheme-complete-or-indent))
+(add-hook 'scheme-mode-hook
+  (lambda ()
+    (make-local-variable 'lisp-indent-function)
+    (setq lisp-indent-function 'scheme-smart-indent-function)
+    (make-local-variable 'eldoc-documentation-function)
+    (setq eldoc-documentation-function 'scheme-get-current-symbol-info)
+    (turn-on-eldoc-mode)))
+
+;;; Paredit
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
 
 (labels ((dumb-indent ()
            (make-local-variable indent-line-function)
