a6108d53236672e4615d5a33e37cdd3e51d96f57
Martin Kuehl <martin.kuehl@gmail.com>
2009-08-14 12:43:49 +0200

    Unify local.el

--- emacs.d/mkhl/local.el
+++ emacs.d/mkhl/local.el
@@ -1,28 +1,39 @@
 
 ;; Kill ring
-(when (require 'browse-kill-ring nil 'noerror)
-  (setq browse-kill-ring-highlight-current-entry t
-        browse-kill-ring-highlight-inserted-item t
-        browse-kill-ring-display-duplicates nil
-        browse-kill-ring-no-duplicates t)
-  (browse-kill-ring-default-keybindings))
+(eval-after-load "browse-kill-ring"
+  '(progn
+     (setq browse-kill-ring-highlight-current-entry t
+           browse-kill-ring-highlight-inserted-item t
+           browse-kill-ring-display-duplicates nil
+           browse-kill-ring-no-duplicates t)
+     (browse-kill-ring-default-keybindings)))
+(require 'browse-kill-ring nil 'noerror)
 
 ;; Centered cursor
-(when (require 'centered-cursor-mode nil 'noerror)
-  (global-centered-cursor-mode t))
+(eval-after-load "centered-cursor-mode"
+  '(progn
+     (global-centered-cursor-mode t)))
+(require 'centered-cursor-mode nil 'noerror)
 
 ;; Color theme
-(when (require 'color-theme nil 'noerror)
-  (color-theme-initialize)
-  (color-theme-xemacs))
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+  (color-theme-xemacs)))
+(require 'color-theme nil 'noerror)
 
 ;; TextMate mode
-(when (fboundp 'textmate-mode)
-  (textmate-mode t)
-  (define-key *textmate-mode-map* [(control c) (control t)] nil)
-  (define-key *textmate-mode-map* [(meta return)] nil))
+(eval-after-load "textmate"
+  '(progn
+     (textmate-mode t)
+     (define-key *textmate-mode-map* [(control c) (control t)] nil)
+     (define-key *textmate-mode-map* [(meta return)] nil)))
+(require 'textmate nil 'noerror)
 
 ;; YASnippet
 (eval-after-load "yasnippet-bundle"
   '(unwind-protect
-       (yas/load-directory (concat dot-emacs-dir "snippets"))))
+       (progn
+         (yas/load-directory (concat dot-emacs-dir "snippets"))
+         (yas/global-mode 1))))
+(require 'yasnippet-bundle nil 'noerror)
