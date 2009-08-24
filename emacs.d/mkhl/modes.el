ccc2abd857124be9a591498e3ac1f97e92f06bfb
Martin Kuehl <martin.kuehl@gmail.com>
2009-08-25 01:17:52 +0200

    Use browse-url for Hoogle

--- emacs.d/mkhl/modes.el
+++ emacs.d/mkhl/modes.el
@@ -76,6 +76,7 @@
     (require 'hs-lint nil 'noerror)
     (local-set-key [(control c) (control h)] 'haskell-hoogle)
     (local-set-key [(control c) (control v)] 'hs-lint)
+    (setq haskell-hoogle-command nil)
     (my/set-indent-to-tab-stops)
     (my/define-tab-width 4)))
 
