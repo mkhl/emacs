43b488a9431b89fdee3e786f7dc9159d1f9ec34c
Martin Kuehl <martin.kuehl@gmail.com>
2009-08-25 19:29:20 +0200

    Don't define the default-face colours in custom.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -22,5 +22,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "grey96" :foreground "grey20" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :family "apple-inconsolata"))))
- )
+ '(default ((t (:stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :family "apple-inconsolata")))))
