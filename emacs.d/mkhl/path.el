ca7b75670e4dd10fe33a8ac8a11da59c2efe3225
Martin Kuehl <martin.kuehl@gmail.com>
2009-09-01 18:56:54 +0200

    Fix exec-path when setting PATH.

--- emacs.d/mkhl/path.el
+++ emacs.d/mkhl/path.el
@@ -4,10 +4,11 @@
        (path-value (shell-command-to-string read-command)))
   (when (string-match (rx (0+ (any ?\r ?\n)) eos) path-value)
     (setq path-value (replace-match "" 'fixed 'literal path-value)))
-  (setenv "PATH" path-value))
+  (setenv "PATH" path-value)
+  (setq exec-path (split-string path-value path-separator)))
 
 ;; Info path fix for Carbon Emacs
 (when (featurep 'carbon-emacs-package)
   (let* ((carbon-emacs-info-directory
-         (concat (file-name-as-directory carbon-emacs-package-prefix) "info")))
+          (concat (file-name-as-directory carbon-emacs-package-prefix) "info")))
     (add-to-list 'Info-directory-list carbon-emacs-info-directory)))
