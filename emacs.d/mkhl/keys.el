d03b199bc93fa9d084bae84faa0c87190443628b
Martin Kuehl <martin.kuehl@gmail.com>
2009-09-01 15:58:04 +0200

    Add a (more) sensible binding for C-z

--- emacs.d/mkhl/keys.el
+++ emacs.d/mkhl/keys.el
@@ -14,7 +14,8 @@
 ;; Undo/Redo
 (when (require 'redo nil 'noerror)
   (global-set-key [(meta z)] 'undo)
-  (global-set-key [(meta Z)] 'redo))
+  (global-set-key [(meta Z)] 'redo)
+  (global-set-key [(control z)] 'zap-to-char))
 
 ;; Shifted motion
 (setq cua-highlight-region-shift-only t)
