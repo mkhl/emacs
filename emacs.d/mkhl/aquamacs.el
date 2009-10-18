daa4a467bf1143cc9c0f1f5d8f76f2b46afe5d4a
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-19 01:41:52 +0200

    Generalize aquamacs.el to work with mac-key-mode.

--- emacs.d/mkhl/aquamacs.el
+++ emacs.d/mkhl/aquamacs.el
@@ -18,12 +18,39 @@
                                 ".*SPEEDBAR.*"
                                 "[ ]?\\*.*\\*[ ]?"))
 
-;; Alt-Control motion
-(define-key osx-key-mode-map [(alt control up)] 'up-list)
-(define-key osx-key-mode-map [(alt control down)] 'down-list)
-(define-key osx-key-mode-map [(alt control left)] 'backward-sexp)
-(define-key osx-key-mode-map [(alt control right)] 'forward-sexp)
+(dolist (pair '((aquamacs osx-key-mode-map)
+                (mac-key-mode mac-key-mode-map)))
+  (destructuring-bind (feature mode-map) pair
+    (eval-after-load feature
+      `(progn
 
-;; Programming convenience
-(define-key osx-key-mode-map [(alt return)] 'my/next-line-and-indent)
-(define-key osx-key-mode-map [(alt \;)] 'comment-or-uncomment-region-or-line)
+         ;; Tiny Mac-like CUA
+         (define-key ,mode-map [(alt x)] 'kill-region)
+         (define-key ,mode-map [(alt c)] 'copy-region-as-kill)
+         (define-key ,mode-map [(alt v)] 'yank)
+         (define-key ,mode-map [(alt shift v)] 'yank-pop)
+
+         ;; Alt-Control motion
+         (define-key ,mode-map [(alt control up)] 'up-list)
+         (define-key ,mode-map [(alt control down)] 'down-list)
+         (define-key ,mode-map [(alt control left)] 'backward-sexp)
+         (define-key ,mode-map [(alt control right)] 'forward-sexp)
+
+         ;; TextMate-like convenience
+         (define-key ,mode-map [(alt return)] 'my/next-line-and-indent)
+         (define-key ,mode-map [(alt shift d)] 'my/duplicate-line-or-region)
+         (define-key ,mode-map [(alt shift k)] 'kill-whole-line)
+         (define-key ,mode-map [(alt shift l)] 'my/mark-line)
+         (define-key ,mode-map [(alt \;)] 'comment-dwim)
+
+         ;; Auto-Pairs (TextMate)
+         (define-key ,mode-map [(alt \()] 'insert-pair)
+         (define-key ,mode-map [(alt \))] 'up-list)
+         (define-key ,mode-map [(alt \[)] 'insert-pair)
+         (define-key ,mode-map [(alt \])] 'up-list)
+         (define-key ,mode-map [(alt \{)] 'insert-pair)
+         (define-key ,mode-map [(alt \})] 'up-list)
+         (define-key ,mode-map [(alt \")] 'insert-pair)
+
+         ;; The end
+         ))))
