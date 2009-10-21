d72ca760f8672911aab18f2238afe79f67d7e266
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-21 22:36:08 +0200

    Make slime aware of my CLs

--- emacs.d/mkhl/modes.el
+++ emacs.d/mkhl/modes.el
@@ -39,6 +39,12 @@
 (eval-after-load "slime"
   '(progn
      (setq slime-net-coding-system 'utf-8-unix)
+     (add-to-list 'slime-lisp-implementations
+                  `(acl (,inferior-lisp-acl-program)) 'append)
+     (add-to-list 'slime-lisp-implementations
+                  `(ccl (,inferior-lisp-ccl-program)) 'append)
+     (add-to-list 'slime-lisp-implementations
+                  `(sbcl (,inferior-lisp-sbcl-program)) 'append)
      (slime-setup '(slime-fancy))))
 
 ;;; Scheme
