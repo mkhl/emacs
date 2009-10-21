ca04fa51d4e2671b6d4dcd9a11e5d2b2752cf2ba
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-21 22:36:19 +0200

    Configure clojure

--- emacs.d/mkhl/modes.el
+++ emacs.d/mkhl/modes.el
@@ -47,6 +47,18 @@
                   `(sbcl (,inferior-lisp-sbcl-program)) 'append)
      (slime-setup '(slime-fancy))))
 
+;;; Clojure
+(when (require 'swank-clojure-autoload nil 'noerror)
+  (swank-clojure-config
+   (let* ((clojure-dir (expand-file-name "~/.clojure/"))
+          (clojure-jar (concat clojure-dir "clojure.jar"))
+          (contrib-jar (concat clojure-dir "clojure-contrib.jar"))
+          (clojure-swank (expand-file-name
+                          (concat swank-clojure-path "../main/clojure/"))))
+     (pushnew contrib-jar swank-clojure-extra-classpaths :test 'equal)
+     (pushnew clojure-swank swank-clojure-extra-classpaths :test 'equal)
+     (setq swank-clojure-jar-path clojure-jar))))
+
 ;;; Scheme
 (setq scheme-program-name "gsi -:d-")
 ;; Gambit.el
