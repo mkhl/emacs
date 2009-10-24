bd582c9d5f49b102bd46d14dea071f908b4cd2ab
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-25 01:05:37 +0200

    Advise autoload generation to be more useful.

---
+++ emacs.d/mkhl/base/autoloads.el
@@ -0,0 +1,6 @@
+
+(defadvice update-file-autoloads (around generate-local-autoloads activate)
+  "Generate autoloads in a file called \"autoloads\" in FILE's directory."
+  (let* ((dir (file-name-as-directory (file-name-directory (ad-get-arg 0))))
+         (generated-autoload-file (concat dir "autoloads.el")))
+    ad-do-it))
