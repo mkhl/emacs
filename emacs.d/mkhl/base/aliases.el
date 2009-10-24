ea18db95277c665d23f38504ed21bfba1a8afdc1
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-25 00:50:58 +0200

    aliases

--- emacs.d/mkhl/base/aliases.el
+++ emacs.d/mkhl/base/aliases.el
@@ -1,5 +1,8 @@
 
+;; sane y-or-n queries
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; shortcuts
 (defalias 'qr 'query-replace)
 (defalias 'qrr 'query-replace-regexp)
 (defalias 'git-status 'magit-status)
