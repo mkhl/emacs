7c6ba45f1e02de0c646f64783148c6a267057882
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-25 02:28:44 +0200

    Fix and extend `eshell' config
    Use C-x C-z to toggle `eshell' instead of launching `shell'

--- emacs.d/mkhl/tools/eshell.el
+++ emacs.d/mkhl/tools/eshell.el
@@ -1,22 +1,16 @@
 
-(setq eshell-save-history-on-exit t
-      eshell-cmpl-cycle-completions nil)
-(let* ((vc-dir-list '("CVS" ".svn" ".git" ".hg" "_darcs"))
-       (vc-regexp `(rx bos (| (: "." (? ".")) ,@vc-dir-list) eos)))
-  (setq eshell-cmpl-dir-ignore (eval vc-regexp)))
+(when (require 'eshell-auto nil 'noerror)
+  (setq eshell-save-history-on-exit t
+        eshell-cmpl-cycle-completions nil
+        eshell-cmpl-ignore-case read-file-name-completion-ignore-case)
+  (let* ((vc-dir-list '("CVS" ".svn" ".git" ".hg" "_darcs"))
+         (vc-regexp `(rx bos (| (: "." (? ".")) ,@vc-dir-list) eos)))
+    (setq eshell-cmpl-dir-ignore (eval vc-regexp)))
+  (eval-after-load 'eshell
+    '(progn
+       (require 'eshell-vc)
+       (defun mk/eshell-mode-hook ()
+         (define-key eshell-mode-map [(control a)] 'eshell-bol)
+         (pushnew 'eshell-handle-ansi-color eshell-output-filter-functions)))))
 
-(eval-after-load 'esh-opt
-  '(progn
-     (require 'em-prompt)
-     (require 'em-term)
-     (require 'em-cmpl)
-     ;; TODO: for some reason requiring this here breaks it, but
-     ;; requiring it after an eshell session is started works fine.
-     ;; (require 'eshell-vc)
-     (defun mk/eshell-mode-hook ()
-       (setenv "PAGER" "cat")
-       (setenv "EDITOR" "emacsclient")
-       (setenv "VISUAL" "emacsclient"))
-     (define-key eshell-mode-map [(control a)] 'eshell-bol)
-     (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
-     (add-hook 'eshell-mode-hook 'mk/eshell-mode-hook)))
+(global-set-key [(control x) (control z)] 'eshell-toggle-cd)
--- emacs.d/mkhl/tools/shell.el
+++ emacs.d/mkhl/tools/shell.el
@@ -2,5 +2,3 @@
 (setq comint-prompt-read-only t)
 
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-(global-set-key [(control x) (control z)] 'shell-current-directory)
