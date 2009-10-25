578c5d5c0669e2f060341caedbde82c564b1b4bd
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-25 02:33:26 +0200

    Load byte-compiled elisp from components when available.

--- emacs.d/mkhl/load-component.el
+++ emacs.d/mkhl/load-component.el
@@ -4,4 +4,4 @@
   (let* ((elisp-dir (file-name-as-directory (concat dot-emacs-dir component)))
          (elisp (rx ".el" eos)))
     (dolist (file (directory-files elisp-dir 'full elisp))
-      (load file))))
+      (load (file-name-sans-extension file)))))
