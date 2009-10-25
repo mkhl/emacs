ecb2f7b579d9846e5e5b0e96974b1e80a780ced0
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-25 19:46:19 +0100

    Set `hippie-expand' functions appropriately

--- emacs.d/mkhl/base/completion.el
+++ emacs.d/mkhl/base/completion.el
@@ -1,2 +1,12 @@
 
+
 (global-set-key [(control tab)] 'hippie-expand)
+
+(setq hippie-expand-try-functions-list '(try-expand-all-abbrevs
+                                         try-expand-list
+                                         try-expand-line
+                                         try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
--- emacs.d/mkhl/extras/yasnippet.el
+++ emacs.d/mkhl/extras/yasnippet.el
@@ -1,4 +1,5 @@
 
 (when (require 'yasnippet-bundle nil 'noerror)
   (yas/load-directory (concat dot-emacs-dir "snippets"))
+  (add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
   (yas/global-mode 1))
