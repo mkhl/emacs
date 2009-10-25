b0bef79d3b2893612f291292723850344c439ef1
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-25 19:45:50 +0100

    Clear aquamacs customizations

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,9 +3,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(aquamacs-autoface-mode nil)
- '(aquamacs-save-options-on-quit nil)
- '(aquamacs-set-creator-codes-after-writing-files nil)
  '(create-frame-min-width 78)
  '(default-major-mode (quote text-mode))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
