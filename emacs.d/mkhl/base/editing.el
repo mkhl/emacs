6d65bce95b5e970fdd0321901d625b04445d0f24
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-25 19:45:35 +0100

    Truncate displayed lines only for file buffers

--- emacs.d/mkhl/base/editing.el
+++ emacs.d/mkhl/base/editing.el
@@ -1,8 +1,11 @@
 
 (setq-default fill-column 72
-              comment-column 40
-              truncate-lines t)
+              comment-column 40)
 
 (setq require-final-newline t
       kill-whole-line t
       comment-auto-fill-only-comments t)
+
+(add-hook 'find-file-hook 'set-truncate-lines)
+(defun set-truncate-lines ()
+  (setq truncate-lines t))
