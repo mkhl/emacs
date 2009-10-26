21838880a2f8d47eb5560602640531728bafdaa2
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-27 00:40:22 +0100

    Tweak completion functions. Use `hippie-expand' on M-/

--- emacs.d/mkhl/base/completion.el
+++ emacs.d/mkhl/base/completion.el
@@ -1,11 +1,12 @@
 
-
 (global-set-key [(control tab)] 'hippie-expand)
+(global-set-key [(meta /)] 'hippie-expand)
 
-(setq hippie-expand-try-functions-list '(try-expand-all-abbrevs
-                                         try-expand-line
-                                         try-expand-dabbrev
-                                         try-expand-dabbrev-all-buffers
-                                         try-expand-dabbrev-from-kill
-                                         try-complete-lisp-symbol-partially
-                                         try-complete-lisp-symbol))
+(setq hippie-expand-try-functions-list
+      '(try-expand-all-abbrevs
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol
+        try-expand-whole-kill))
