71ce34b76a5a16cebfa8734d2daaf55e650c9f9a
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-26 13:48:18 +0100

    Don't try to complete lists.
    I  usually `insert-pair'  the parens,  so completing  a list  inserts an
    extra closing paren. Which sucks. :-)

--- emacs.d/mkhl/base/completion.el
+++ emacs.d/mkhl/base/completion.el
@@ -3,7 +3,6 @@
 (global-set-key [(control tab)] 'hippie-expand)
 
 (setq hippie-expand-try-functions-list '(try-expand-all-abbrevs
-                                         try-expand-list
                                          try-expand-line
                                          try-expand-dabbrev
                                          try-expand-dabbrev-all-buffers
