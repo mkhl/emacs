1f48a89e931f961caa84944c006aea064e2a935c
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-26 01:40:04 +0100

    Truncate in dired buffers as well

--- emacs.d/mkhl/base/editing.el
+++ emacs.d/mkhl/base/editing.el
@@ -7,5 +7,6 @@
       comment-auto-fill-only-comments t)
 
 (add-hook 'find-file-hook 'set-truncate-lines)
+(add-hook 'dired-mode-hook 'set-truncate-lines)
 (defun set-truncate-lines ()
   (setq truncate-lines t))
