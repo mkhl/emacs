6530a8aac0bdbb9601f85b928a56ef1c94c42ab8
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-26 13:44:55 +0100

    Remove the deprecated code from `insert-pair-or-skip'

--- emacs.d/mkhl/extras/autopairs.el
+++ emacs.d/mkhl/extras/autopairs.el
@@ -3,14 +3,11 @@
   "When looking at the character to be inserted, skip over it.
 Otherwise call `insert-pair', which see."
   (interactive)
-  (labels ((looking-at-char? (char)
-             (and (characterp char) (looking-at (char-to-string char)))))
-    (let* ((char1 last-command-char)
-           (char2 (event-basic-type last-command-event)))
-      (if (or (looking-at-char? char1)
-              (looking-at-char? char2))
-          (call-interactively 'forward-char)
-        (call-interactively 'insert-pair)))))
+  (let* ((char (event-basic-type last-command-event)))
+    (if (and (characterp char)
+             (looking-at (char-to-string char)))
+        (call-interactively 'forward-char)
+      (call-interactively 'insert-pair))))
 
 (setq parens-require-spaces nil)
 
