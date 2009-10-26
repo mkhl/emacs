5c1ff8d29d9ae487639ecc7a94cf079ed7725415
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-26 01:42:08 +0100

    Prefer a real hiding function to the AppleScript crutch when available

--- emacs.d/mkhl/base/hiding.el
+++ emacs.d/mkhl/base/hiding.el
@@ -1,10 +1,13 @@
 
-(defun mac-hide-emacs ()
-  (interactive)
-  (do-applescript (format "tell application \"System Events\"
+(if (fboundp 'ns-do-hide-emacs)
+    (defalias 'mac-hide-emacs 'ns-do-hide-emacs)
+  (defun mac-hide-emacs ()
+    (interactive)
+    (do-applescript (format "\
+tell application \"System Events\"
     tell application process \"%s\"
         set visible to false
     end tell
-end tell" invocation-name)))
+end tell" invocation-name))))
 
 (global-set-key [(control meta shift h)] 'mac-hide-emacs)
