2f35b537a5f351cebd2b67b334dccdb71bb7f032
Martin Kuehl <martin.kuehl@gmail.com>
2009-10-26 13:36:34 +0100

    Go back to truncating all buffers

--- emacs.d/mkhl/base/editing.el
+++ emacs.d/mkhl/base/editing.el
@@ -1,12 +1,8 @@
 
 (setq-default fill-column 72
-              comment-column 40)
+              comment-column 40
+              truncate-lines t)
 
 (setq require-final-newline t
       kill-whole-line t
       comment-auto-fill-only-comments t)
-
-(add-hook 'find-file-hook 'set-truncate-lines)
-(add-hook 'dired-mode-hook 'set-truncate-lines)
-(defun set-truncate-lines ()
-  (setq truncate-lines t))
