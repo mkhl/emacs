744adb2044d3a6487c6828eadd75175083b8499c
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-03 18:10:20 +0100

    Use the swank-clojure jar file

--- emacs.d/mkhl/modes/clojure.el
+++ emacs.d/mkhl/modes/clojure.el
@@ -4,8 +4,7 @@
    (let* ((clojure-dir (expand-file-name "~/.clojure/"))
           (clojure-jar (concat clojure-dir "clojure.jar"))
           (contrib-jar (concat clojure-dir "clojure-contrib.jar"))
-          (clojure-swank (expand-file-name
-                          (concat swank-clojure-path "../main/clojure/"))))
+          (clojure-swank (concat clojure-dir "swank-clojure.jar")))
      (pushnew contrib-jar swank-clojure-extra-classpaths :test 'equal)
      (pushnew clojure-swank swank-clojure-extra-classpaths :test 'equal)
      (setq swank-clojure-jar-path clojure-jar))))
