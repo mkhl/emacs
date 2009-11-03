640a01c3b31c7c75a0167a8f25925a5a86e2527e
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-03 18:11:04 +0100

    Implement indenting yanked text in some modes

--- emacs.d/mkhl/base/yanking.el
+++ emacs.d/mkhl/base/yanking.el
@@ -1,22 +1,23 @@
 
-;; Indent yanked text
-;; - This doesn't work because the defadvice bodies are only executed
-;;   when the advised function was called, i.e. outside of the lexical
-;;   environment defined with let and labels.
-;; - Workaround: Code duplication. Whoopdeedoo.
-;; (let ((indenting-modes '(emacs-lisp-mode
-;;                          scheme-mode
-;;                          lisp-mode
-;;                          c-mode
-;;                          c++-mode
-;;                          objc-mode
-;;                          latex-mode
-;;                          plain-tex-mode)))
-;;   (labels ((indent-yanked-region ()
-;;              (when (member major-mode indenting-modes)
-;;                (let ((mark-even-if-inactive t))
-;;                  (indent-region (region-beginning) (region-end) nil)))))
-;;     (defadvice yank (after indent-region activate)
-;;       (funcall #'indent-yanked-region))
-;;     (defadvice yank-pop (after indent-region activate)
-;;       (funcall #'indent-yanked-region))))
+(defvar indent-region-modes '(emacs-lisp-mode
+                              lisp-interaction-mode
+                              lisp-mode
+                              scheme-mode
+                              clojure-mode
+                              c-mode
+                              c++-mode
+                              objc-mode)
+  "List of modes that support smart indentation of the region.")
+
+(defun indent-yanked-region ()
+  (when (member major-mode indent-region-modes)
+    (let* ((mark-even-if-inactive t))
+      (indent-region (region-beginning) (region-end)))))
+
+(defadvice yank (after indent-region activate)
+  "Indent yanked text if `major-mode' supports it."
+  (indent-yanked-region))
+
+(defadvice yank-pop (after indent-region activate)
+  "Indent yanked text if `major-mode' supports it."
+  (indent-yanked-region))
