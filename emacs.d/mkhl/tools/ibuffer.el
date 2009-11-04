537a77ea17e31a3b5497bb2308aaf01548812006
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-04 16:44:48 +0100

    Next thing you know I'll actually test changes before I push them...

--- emacs.d/mkhl/tools/ibuffer.el
+++ emacs.d/mkhl/tools/ibuffer.el
@@ -7,21 +7,20 @@
   (ibuffer-switch-to-saved-filter-groups "mkhl"))
 
 (defun mk/eval-after-ibuffer ()
+  (setq ibuffer-saved-filter-groups
+        `(("mkhl"
+           ("Emacs" (or (name . ,(rx bos "*scratch*" eos))
+                        (name . ,(rx bos "*Messages*" eos))
+                        (name . ,(rx bos "*Completions*" eos))
+                        (name . ,(rx bos "*Help*" eos))
+                        (name . ,(rx bos "*Apropos*" eos))
+                        (name . ,(rx bos "*Shell Command Output*" eos))
+                        (name . ,(rx bos "*vc*" eos))))
+           ("Magit" (name . ,(rx bos "*magit")))
+           ("Eshell" (mode . eshell-mode))
+           ("Dired" (mode . dired-mode)))))
   (define-key ibuffer-mode-map (kbd "TAB") 'ibuffer-toggle-filter-group)
   (add-hook 'ibuffer-mode-hook 'mk/ibuffer-mode-hook))
 
 (eval-after-load 'ibuffer
   '(mk/eval-after-ibuffer))
-
-(add-to-list 'ibuffer-saved-filter-groups
-             `("mkhl"
-               ("Emacs" (or (name . ,(rx bos "*scratch*" eos))
-                            (name . ,(rx bos "*Messages*" eos))
-                            (name . ,(rx bos "*Completions*" eos))
-                            (name . ,(rx bos "*Help*" eos))
-                            (name . ,(rx bos "*Apropos*" eos))
-                            (name . ,(rx bos "*Shell Command Output*" eos))
-                            (name . ,(rx bos "*vc*" eos))))
-               ("Eshell" (mode . eshell-mode))
-               ("Magit" (name . ,(rx bos "*magit")))
-               ("Dired" (mode . dired-mode))))
