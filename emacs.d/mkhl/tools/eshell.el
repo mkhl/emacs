4bfe78bd0f8a195f49cc1b6298445547c803ce99
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-04 02:14:49 +0100

    Make `eshell' config more readable

--- emacs.d/mkhl/tools/eshell.el
+++ emacs.d/mkhl/tools/eshell.el
@@ -1,4 +1,15 @@
 
+(defun mk/eshell-mode-hook ()
+  (setenv "TERM" "ansi")
+  (pushnew 'eshell-handle-ansi-color eshell-output-filter-functions)
+  (define-key eshell-mode-map [(control a)] 'eshell-bol))
+
+(defun mk/eval-after-eshell ()
+  (add-hook 'eshell-mode-hook 'mk/eshell-mode-hook))
+
+(eval-after-load 'eshell
+  '(mk/eval-after-eshell))
+
 (when (require 'eshell-auto nil 'noerror)
   (setq eshell-save-history-on-exit t
         eshell-cmpl-cycle-completions nil
@@ -6,12 +17,4 @@
   (let* ((vc-dir-list '("CVS" ".svn" ".git" ".hg" "_darcs"))
          (vc-regexp `(rx bos (| (: "." (? ".")) ,@vc-dir-list) eos)))
     (setq eshell-cmpl-dir-ignore (eval vc-regexp)))
-  (eval-after-load 'eshell
-    '(progn
-       (defun mk/eshell-mode-hook ()
-         (setenv "TERM" "ansi")
-         (define-key eshell-mode-map [(control a)] 'eshell-bol)
-         (pushnew 'eshell-handle-ansi-color eshell-output-filter-functions))
-       (add-hook 'eshell-mode-hook 'mk/eshell-mode-hook))))
-
-(global-set-key [(control x) (control z)] 'eshell-toggle)
+  (global-set-key [(control x) (control z)] 'eshell-toggle))
