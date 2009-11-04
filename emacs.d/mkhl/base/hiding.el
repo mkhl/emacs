cc35a454d28538f378b03c86c8d7489a36c507c0
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-04 01:55:23 +0100

    Remove obsolete code

--- emacs.d/mkhl/base/hiding.el
+++ emacs.d/mkhl/base/hiding.el
@@ -1,13 +1,2 @@
 
-(if (fboundp 'ns-do-hide-emacs)
-    (defalias 'mac-hide-emacs 'ns-do-hide-emacs)
-  (defun mac-hide-emacs ()
-    (interactive)
-    (do-applescript (format "\
-tell application \"System Events\"
-    tell application process \"%s\"
-        set visible to false
-    end tell
-end tell" invocation-name))))
-
-(global-set-key [(control meta shift h)] 'mac-hide-emacs)
+(global-set-key [(control meta shift h)] 'ns-do-hide-emacs)
