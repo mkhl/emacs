bbd198f378b37ab7ed62a1e22d3f93bfd7253bee
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-04 16:37:15 +0100

    Define sensible custom `ibuffer' filter groups

--- emacs.d/mkhl/tools/ibuffer.el
+++ emacs.d/mkhl/tools/ibuffer.el
@@ -4,7 +4,7 @@
       ibuffer-show-empty-filter-groups nil)
 
 (defun mk/ibuffer-mode-hook ()
-  (ibuffer-set-filter-groups-by-mode))
+  (ibuffer-switch-to-saved-filter-groups "mkhl"))
 
 (defun mk/eval-after-ibuffer ()
   (define-key ibuffer-mode-map (kbd "TAB") 'ibuffer-toggle-filter-group)
@@ -12,3 +12,16 @@
 
 (eval-after-load 'ibuffer
   '(mk/eval-after-ibuffer))
+
+(add-to-list 'ibuffer-saved-filter-groups
+             `("mkhl"
+               ("Emacs" (or (name . ,(rx bos "*scratch*" eos))
+                            (name . ,(rx bos "*Messages*" eos))
+                            (name . ,(rx bos "*Completions*" eos))
+                            (name . ,(rx bos "*Help*" eos))
+                            (name . ,(rx bos "*Apropos*" eos))
+                            (name . ,(rx bos "*Shell Command Output*" eos))
+                            (name . ,(rx bos "*vc*" eos))))
+               ("Eshell" (mode . eshell-mode))
+               ("Magit" (name . ,(rx bos "*magit")))
+               ("Dired" (mode . dired-mode))))
