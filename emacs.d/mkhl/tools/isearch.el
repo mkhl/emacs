f97cd14f11298d3016730930e85cdde0b1d29525
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-04 02:14:20 +0100

    Make `isearch' config more readable

--- emacs.d/mkhl/tools/isearch.el
+++ emacs.d/mkhl/tools/isearch.el
@@ -4,17 +4,19 @@
   (when (and isearch-forward isearch-other-end)
     (goto-char isearch-other-end)))
 
+(defun mk/eval-after-isearch ()
+  (global-set-key [(control s)] 'isearch-forward)
+  (global-set-key [(control S)] 'isearch-backward)
+  (global-set-key [(control meta s)] 'isearch-forward-regexp)
+  (global-set-key [(control meta S)] 'isearch-backward-regexp)
+  (define-key isearch-mode-map [(control s)] 'isearch-repeat-forward)
+  (define-key isearch-mode-map [(control S)] 'isearch-repeat-backward)
+  (define-key isearch-mode-map [(control meta s)]
+    'isearch-repeat-forward-regexp)
+  (define-key isearch-mode-map [(control meta S)]
+    'isearch-repeat-backward-regexp)
+  (global-set-key [(control *)] 'isearch-forward-at-point)
+  (add-hook 'isearch-mode-end-hook 'isearch-goto-other-end))
+
 (eval-after-load "isearch"
-  '(progn
-     (global-set-key [(control s)] 'isearch-forward)
-     (global-set-key [(control S)] 'isearch-backward)
-     (global-set-key [(control meta s)] 'isearch-forward-regexp)
-     (global-set-key [(control meta S)] 'isearch-backward-regexp)
-     (define-key isearch-mode-map [(control s)] 'isearch-repeat-forward)
-     (define-key isearch-mode-map [(control S)] 'isearch-repeat-backward)
-     (define-key isearch-mode-map [(control meta s)]
-       'isearch-repeat-forward-regexp)
-     (define-key isearch-mode-map [(control meta S)]
-       'isearch-repeat-backward-regexp)
-     (global-set-key [(control *)] 'isearch-forward-at-point)
-     (add-hook 'isearch-mode-end-hook 'isearch-goto-other-end)))
+  '(mk/eval-after-isearch))
