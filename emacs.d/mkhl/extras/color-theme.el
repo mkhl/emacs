817bec9f8f4815614c1261c2859ecd20d9323809
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-05 14:03:53 +0100

    Abstract and simplify color-theme loading code.

--- emacs.d/mkhl/extras/color-theme.el
+++ emacs.d/mkhl/extras/color-theme.el
@@ -2,9 +2,9 @@
 (when (require 'color-theme nil 'noerror)
   (color-theme-initialize)
   (eval-after-init
-    (cond ((fboundp 'color-theme-quiet-light)
-           (color-theme-quiet-light))
-          ((fboundp 'color-theme-espresso)
-           (color-theme-espresso))
-          (t (color-theme-xemacs)
-             (set-face-background 'default "#F5F5F5")))))
+    (dolist (theme '(color-theme-quiet-light
+                     color-theme-espresso
+                     color-theme-gtk-ide))
+      (when (fboundp theme)
+        (funcall theme)
+        (return)))))
