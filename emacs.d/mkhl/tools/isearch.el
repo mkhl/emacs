f92c9d3d68933eaf39d5ce328fa0d99c5c85802b
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-06 16:50:21 +0100

    Fix `isearch' bindings

--- emacs.d/mkhl/tools/isearch.el
+++ emacs.d/mkhl/tools/isearch.el
@@ -6,13 +6,13 @@
 
 (defun mk/eval-after-isearch ()
   (global-set-key (kbd "C-s") 'isearch-forward)
-  (global-set-key (kbd "C-S") 'isearch-backward)
+  (global-set-key (kbd "C-S-s") 'isearch-backward)
   (global-set-key (kbd "C-M-s") 'isearch-forward-regexp)
-  (global-set-key (kbd "C-M-S") 'isearch-backward-regexp)
+  (global-set-key (kbd "C-M-S-s") 'isearch-backward-regexp)
   (define-key isearch-mode-map (kbd "C-s") 'isearch-repeat-forward)
-  (define-key isearch-mode-map (kbd "C-S") 'isearch-repeat-backward)
+  (define-key isearch-mode-map (kbd "C-S-s") 'isearch-repeat-backward)
   (define-key isearch-mode-map (kbd "C-M-s") 'isearch-repeat-forward-regexp)
-  (define-key isearch-mode-map (kbd "C-M-S") 'isearch-repeat-backward-regexp)
+  (define-key isearch-mode-map (kbd "C-M-S-s") 'isearch-repeat-backward-regexp)
   (add-hook 'isearch-mode-end-hook 'isearch-goto-other-end))
 
 (eval-after-load "isearch"
