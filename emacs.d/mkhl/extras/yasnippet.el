f3bec0b9c1a41e79ebccec0cb620761c53c4e45d
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-06 22:01:32 +0100

    Define function to locally fix `yas/trigger-key' overrides

--- emacs.d/mkhl/extras/yasnippet.el
+++ emacs.d/mkhl/extras/yasnippet.el
@@ -4,3 +4,7 @@
   (add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
   (yas/load-directory (concat dot-emacs-dir "snippets"))
   (yas/global-mode 1))
+
+(defun yas/fix-trigger-key ()
+  (setq yas/fallback-behavior `(apply ,(local-key-binding (kbd "TAB"))))
+  (local-set-key (kbd "<tab>") 'yas/expand))
