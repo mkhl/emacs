eb1f15034a6e500ea00e656016d62396b0036de0
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-06 13:52:58 +0100

    Hey, let's change truncation behaviour again!

--- emacs.d/mkhl/base/editing.el
+++ emacs.d/mkhl/base/editing.el
@@ -1,8 +1,12 @@
 
 (setq-default fill-column 72
-              comment-column 40
-              truncate-lines t)
+              comment-column 40)
 
 (setq require-final-newline t
       kill-whole-line t
       comment-auto-fill-only-comments t)
+
+(add-hook 'find-file-hook 'do-truncate-lines)
+(add-hook 'dired-mode-hook 'do-truncate-lines)
+(defun do-truncate-lines ()
+  (setq truncate-lines t))
