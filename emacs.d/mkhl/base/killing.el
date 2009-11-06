0fcbc2a1acc5a9c55b8e06e1223c7a220bdc7f99
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-06 21:50:13 +0100

    Confirm killing Emacs with 5 second timeout
    Idea borrowed from Greg Newman :-)

--- emacs.d/mkhl/base/killing.el
+++ emacs.d/mkhl/base/killing.el
@@ -4,3 +4,10 @@
   (if (and (eolp) (not (bolp)))
       (delete-indentation t)
     ad-do-it))
+
+(defun kill-emacs-with-timeout (prompt)
+  (let* ((message "will exit automatically in 5 seconds")
+         (prompt (format "%s(%s) " prompt message)))
+    (y-or-n-p-with-timeout prompt 5 t)))
+
+(setq confirm-kill-emacs 'kill-emacs-with-timeout)
