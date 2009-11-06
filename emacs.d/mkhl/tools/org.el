840c2048bcf6562eb216dac1c585e64937c29ca1
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-06 13:53:16 +0100

    Use `ido' for org completions

--- emacs.d/mkhl/tools/org.el
+++ emacs.d/mkhl/tools/org.el
@@ -1,5 +1,6 @@
 
 (when (require 'org-install nil 'noerror)
   (require 'calendar)
-  (setq org-replace-disputed-keys t)
+  (setq org-replace-disputed-keys t
+        org-completion-use-ido t)
   (add-to-list 'auto-mode-alist `(,(rx ".org" eos) . org-mode)))
