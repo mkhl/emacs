57ae5a6736cc048118f7b4b3cc46ac1ccaa4cc72
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-06 11:53:09 +0100

    Revert "Remove obsolete code"
    This reverts commit cc35a454d28538f378b03c86c8d7489a36c507c0.

--- emacs.d/mkhl/base/hiding.el
+++ emacs.d/mkhl/base/hiding.el
@@ -1,2 +1,13 @@
 
-(global-set-key (kbd "C-M-H") 'ns-do-hide-emacs)
+(if (fboundp 'ns-do-hide-emacs)
+    (defalias 'mac-hide-emacs 'ns-do-hide-emacs)
+  (defun mac-hide-emacs ()
+    (interactive)
+    (do-applescript (format "\
+tell application \"System Events\"
+    tell application process \"%s\"
+        set visible to false
+    end tell
+end tell" invocation-name))))
+
+(global-set-key [(control meta shift h)] 'mac-hide-emacs)
