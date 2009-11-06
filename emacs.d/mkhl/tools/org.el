abd600acbbc3252eee0de6893dd9e27099dc01c8
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-06 13:53:31 +0100

    Add a few global org bindings

--- emacs.d/mkhl/tools/org.el
+++ emacs.d/mkhl/tools/org.el
@@ -3,4 +3,10 @@
   (require 'calendar)
   (setq org-replace-disputed-keys t
         org-completion-use-ido t)
+  (let* ((prefix-key "C-x C-o"))
+    (labels ((org-kbd (key) (read-kbd-macro (format "%s %s" prefix-key key))))
+      (global-unset-key (read-kbd-macro prefix-key))
+      (global-set-key (org-kbd "l") 'org-store-link)
+      (global-set-key (org-kbd "a") 'org-agenda)
+      (global-set-key (org-kbd "b") 'org-iswitchb)))
   (add-to-list 'auto-mode-alist `(,(rx ".org" eos) . org-mode)))
