c6e8353390392f0c31945bc804c4fe04db211a76
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-07 19:22:07 +0100

    Add global M-n/p bindings

--- emacs.d/mkhl/base/motion.el
+++ emacs.d/mkhl/base/motion.el
@@ -3,3 +3,5 @@
 (global-set-key (kbd "<C-right>") 'forward-word)
 (global-set-key (kbd "<M-up>") 'backward-paragraph)
 (global-set-key (kbd "<M-down>") 'forward-paragraph)
+(global-set-key (kbd "M-p") 'backward-paragraph)
+(global-set-key (kbd "M-n") 'forward-paragraph)
