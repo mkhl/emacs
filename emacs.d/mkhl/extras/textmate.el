d5f4b610c176f4812da5cccb259f0becae830992
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-10 15:33:41 +0100

    Bind M-l to `goto-line'

--- emacs.d/mkhl/extras/textmate.el
+++ emacs.d/mkhl/extras/textmate.el
@@ -3,6 +3,7 @@
 (global-set-key (kbd "M-RET") 'next-line-and-indent)
 (global-set-key (kbd "M-D") 'duplicate-line-or-region)
 (global-set-key (kbd "M-K") 'kill-whole-line)
+(global-set-key (kbd "M-l") 'goto-line)
 (global-set-key (kbd "M-L") 'mark-line)
 (global-set-key (kbd "M-q") 'fill-paragraph-or-region)
 (global-set-key (kbd "M-u") 'upcase-word-or-region)
