a915f6f54ece6c5429c274c130c1de7c2c80634c
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-10 15:31:01 +0100

    Add `once-only'

--- emacs.d/mkhl/helpers/cl-ext.el
+++ emacs.d/mkhl/helpers/cl-ext.el
@@ -1,18 +1,25 @@
 
 (defmacro* with-gensyms ((&rest names) &body body)
   "Evaluate BODY with each NAME bound to a `gensym'.
-
-\(fn (NAME ...) BODY...)"
+\n(fn (NAME ...) BODY...)"
   (declare (indent 1))
   `(let ,(loop for n in names collect `(,n (gensym)))
      ,@body))
 
+(defmacro* once-only ((&rest names) &body body)
+  "Evaluate BODY with each NAME evaluated exactly once beforehand.
+\n(fn (NAME ...) BODY...)"
+  (declare (indent 1))
+  (let ((gensyms (loop for n in names collect (gensym))))
+    `(let (,@(loop for g in gensyms collect `(,g (gensym))))
+       `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
+          ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
+             ,@body)))))
+
 (defmacro* dolist* ((args list &optional result) &body body)
   "Loop over a list.
-
-Like normal `dolist', except ARGS allows full Common Lisp conventions.
-
-\(fn (ARGS LIST [RESULT]) BODY...)"
+\nLike normal `dolist', except ARGS allows full Common Lisp conventions.
+\n(fn (ARGS LIST [RESULT]) BODY...)"
   (declare (indent 1))
   (with-gensyms (loop-var)
     `(dolist (,loop-var ,list ,result)
