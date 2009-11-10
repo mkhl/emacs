d3bf3bf90da4645613fd12a8d038a650691a9b0d
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-10 15:01:12 +0100

    Define more helpful elisp keys
    But only bind them in elisp modes

--- emacs.d/mkhl/modes/emacs-lisp.el
+++ emacs.d/mkhl/modes/emacs-lisp.el
@@ -4,6 +4,14 @@
   (when (fboundp 'highlight-parentheses-mode)
     (highlight-parentheses-mode t)))
 
+(defun mk/emacs-lisp-keys ()
+  (dolist (mode-map (list emacs-lisp-mode-map
+                          lisp-interaction-mode-map))
+    (define-key mode-map (kbd "C-x F") #'find-function)
+    (define-key mode-map (kbd "C-x K") #'find-function-on-key)
+    (define-key mode-map (kbd "C-x L") #'find-library)
+    (define-key mode-map (kbd "C-x V") #'find-variable)))
+
 (defun mk/eval-after-lisp-mode ()
   (dolist (mode-map (list emacs-lisp-mode-map
                           lisp-interaction-mode-map
@@ -11,11 +19,9 @@
     (define-key mode-map (kbd "M-(") 'insert-parentheses)
     (define-key mode-map (kbd "M-)") 'move-past-close-and-reindent)
     (define-key mode-map (kbd "<C-tab>") 'lisp-complete-symbol))
+  (mk/emacs-lisp-keys)
   (add-hook 'emacs-lisp-mode-hook 'mk/emacs-lisp-mode-hook)
   (add-hook 'lisp-interaction-mode-hook 'mk/emacs-lisp-mode-hook))
 
 (eval-after-load 'lisp-mode
   '(mk/eval-after-lisp-mode))
-
-(global-set-key (kbd "C-x F") 'find-function)
-(global-set-key (kbd "C-x V") 'find-variable)
