1fbee21c3d1e266bb065a4a05bee3488a27aaee9
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-13 13:20:12 +0100

    Restructure `eshell' setup

--- emacs.d/mkhl/tools/eshell.el
+++ emacs.d/mkhl/tools/eshell.el
@@ -4,17 +4,17 @@
   (pushnew 'eshell-handle-ansi-color eshell-output-filter-functions)
   (define-key eshell-mode-map (kbd "C-a") 'eshell-bol))
 
-(defun mk/eval-after-eshell ()
-  (add-hook 'eshell-mode-hook 'mk/eshell-mode-hook))
-
-(eval-after-load 'eshell
-  '(mk/eval-after-eshell))
-
-(when (require 'eshell-auto nil 'noerror)
+(defun mk/setup-eshell ()
   (setq eshell-save-history-on-exit t
         eshell-cmpl-cycle-completions nil
         eshell-cmpl-ignore-case read-file-name-completion-ignore-case)
   (let* ((vc-dir-list '("CVS" ".svn" ".git" ".hg" "_darcs"))
          (vc-regexp `(rx bos (| (: "." (? ".")) ,@vc-dir-list) eos)))
     (setq eshell-cmpl-dir-ignore (eval vc-regexp)))
+  (add-hook 'eshell-mode-hook 'mk/eshell-mode-hook))
+
+(eval-after-load 'eshell
+  '(mk/setup-eshell))
+
+(when (require 'eshell-auto nil 'noerror)
   (global-set-key (kbd "C-x C-z") 'eshell-toggle))
