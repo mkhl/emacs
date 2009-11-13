d831a0a34b2fe17041ab3c6331132eddff4c79d0
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-13 12:57:17 +0100

    Tweak emacs-lisp bindings

--- emacs.d/mkhl/modes/emacs-lisp.el
+++ emacs.d/mkhl/modes/emacs-lisp.el
@@ -4,6 +4,9 @@
   (when (fboundp 'highlight-parentheses-mode)
     (highlight-parentheses-mode t)))
 
+(defun mk/lisp-interaction-keys ()
+  (define-key lisp-interaction-mode-map (kbd "C-S-j") 'eval-print-last-sexp))
+
 (defun mk/emacs-lisp-keys ()
   (dolist (mode-map (list emacs-lisp-mode-map
                           lisp-interaction-mode-map))
@@ -12,14 +15,18 @@
     (define-key mode-map (kbd "C-x L") #'find-library)
     (define-key mode-map (kbd "C-x V") #'find-variable)))
 
-(defun mk/eval-after-lisp-mode ()
+(defun mk/keys ()
   (dolist (mode-map (list emacs-lisp-mode-map
                           lisp-interaction-mode-map
                           lisp-mode-map))
     (define-key mode-map (kbd "M-(") 'insert-parentheses)
     (define-key mode-map (kbd "M-)") 'move-past-close-and-reindent)
-    (define-key mode-map (kbd "<C-tab>") 'lisp-complete-symbol))
+    (define-key mode-map (kbd "<C-tab>") 'lisp-complete-symbol)))
+
+(defun mk/eval-after-lisp-mode ()
+  (mk/lisp-keys)
   (mk/emacs-lisp-keys)
+  (mk/lisp-interaction-keys)
   (add-hook 'emacs-lisp-mode-hook 'mk/emacs-lisp-mode-hook)
   (add-hook 'lisp-interaction-mode-hook 'mk/emacs-lisp-mode-hook))
 
