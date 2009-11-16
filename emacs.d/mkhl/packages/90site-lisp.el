5e70e63e980495624fbcb7a37ed6bd16c5a80d88
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-17 00:52:28 +0100

    Load `site-lisp' libraries from their respective subdirectories

--- emacs.d/mkhl/packages/90site-lisp.el
+++ emacs.d/mkhl/packages/90site-lisp.el
@@ -2,6 +2,8 @@
 ;; Load packages from the `site-lisp' subdirectory
 (let* ((site-lisp-dir (file-name-as-directory
                        (concat dot-emacs-dir "site-lisp")))
+       (non-hidden (rx bos (not (in "."))))
        (autoloads (concat site-lisp-dir "autoloads")))
-  (add-to-list 'load-path site-lisp-dir)
+  (dolist (path (directory-files site-lisp-dir 'full non-hidden))
+    (add-to-list 'load-path (file-name-as-directory path)))
   (load autoloads 'noerror))
