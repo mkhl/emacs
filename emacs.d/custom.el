655b8bf8cea27f86580e687f4ac5bcded8979aee
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-16 02:17:50 +0100

    Update default face for Emacs 23

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -30,4 +30,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :family "apple-menlo")))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Menlo")))))
