373ef407fbf541d8cc62568ca2b507ea05a0796c
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-24 03:30:08 +0100

    Support `smart-tab' in `org-mode'

--- emacs.d/mkhl/extras/smart-tab.el
+++ emacs.d/mkhl/extras/smart-tab.el
@@ -2,6 +2,13 @@
 (when (require 'smart-tab nil 'noerror)
   (global-smart-tab-mode t))
 
+(defun mk/setup-smart-tab-org ()
+  (defadvice smart-tab-default (around smart-tab-use-org-cycle activate)
+    "In `org-mode', use `org-cycle' instead of indenting."
+    (case major-mode
+      ('org-mode (org-cycle current-prefix-arg))
+      (otherwise ad-do-it))))
+
 (defun mk/setup-smart-tab-eshell ()
   (pushnew '(eshell-mode . pcomplete)
            smart-tab-completion-functions-alist))
@@ -17,6 +24,7 @@
 
 (defun mk/eval-after-smart-tab ()
   (setq smart-tab-using-hippie-expand t)
+  (mk/setup-smart-tab-org)
   (mk/setup-smart-tab-eshell)
   (mk/setup-smart-tab-shell)
   (mk/setup-smart-tab-scheme-complete))
