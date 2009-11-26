6057bde0d888fe806f316e9e764bc79c3b08f953
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-26 13:28:16 +0100

    Clear up the names

--- emacs.d/mkhl/load-component.el
+++ emacs.d/mkhl/load-component.el
@@ -2,6 +2,6 @@
 (defun load-component (component)
   "Load elisp sources from COMPONENT."
   (let* ((elisp-dir (file-name-as-directory (concat dot-emacs-dir component)))
-         (elisp (rx ".el" eos)))
-    (dolist (file (directory-files elisp-dir 'full elisp))
-      (load (file-name-sans-extension file)))))
+         (elisp-rx (rx ".el" eos)))
+    (dolist (file (directory-files elisp-dir 'full elisp-rx))
+      (load (file-name-sans-extension file) nil 'nomessage))))
