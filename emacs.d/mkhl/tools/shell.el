43d90ae7b6c5e843d062499a64752e9a56ac5511
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-26 10:55:05 +0100

    Extend `shell' configuration

--- emacs.d/mkhl/tools/shell.el
+++ emacs.d/mkhl/tools/shell.el
@@ -1,4 +1,22 @@
 
-(setq comint-prompt-read-only t)
+;;; `comint'
 
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(defun mk/setup-comint ()
+  (setq comint-prompt-read-only t)
+  (add-hook 'comint-mode-hook 'ansi-color-for-comint-mode-on))
+
+(eval-after-load 'comint
+  '(mk/setup-comint))
+
+;;; `shell'
+
+(defun mk/setup-shell ()
+  (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on))
+
+(eval-after-load 'shell
+  '(mk/setup-shell))
+
+;;; `shell-current-directory'
+
+(when (fboundp 'shell-current-directory)
+  (global-set-key (kbd "C-x C-l") 'shell-current-directory))
