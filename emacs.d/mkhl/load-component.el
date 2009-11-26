5b8725a0ca5ea7c7a48b8837b12e523d487351d8
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-26 13:41:30 +0100

    Uncompact `load-component' source

--- emacs.d/mkhl/load-component.el
+++ emacs.d/mkhl/load-component.el
@@ -1,7 +1,8 @@
 
 (defun load-component (component)
   "Load elisp sources from COMPONENT."
-  (let* ((elisp-dir (file-name-as-directory (concat dot-emacs-dir component)))
+  (let* ((elisp-src (concat dot-emacs-dir component))
+         (elisp-dir (file-name-as-directory elisp-src))
          (elisp-rx (rx ".el" eos)))
     (dolist (file (directory-files elisp-dir 'full elisp-rx))
       (load (file-name-sans-extension file) nil 'nomessage))))
