e8e82a4848f84780e8504c67968156147d7b2016
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-26 12:35:45 +0100

    More builtin packages

--- emacs.d/mkhl/packages/00builtin.el
+++ emacs.d/mkhl/packages/00builtin.el
@@ -1,4 +1,7 @@
 
+;; Just to be /completely/ safe...
+(require 'cl)
+(require 'autoload)
 ;; Make sure built-in packages like `generic-x' can be overridden.
 (require 'generic-x)
 (require 'jka-compr)
