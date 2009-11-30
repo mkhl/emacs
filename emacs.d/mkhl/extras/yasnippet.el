61a3e8617f4c6fd49320ccec722332e34527627a
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-30 13:55:37 +0100

    Reorganize and extend yasnippet configuration.

--- emacs.d/mkhl/extras/yasnippet.el
+++ emacs.d/mkhl/extras/yasnippet.el
@@ -1,9 +1,28 @@
 
-(when (require 'yasnippet-bundle nil 'noerror)
+(defun mk/yasnippet-keys ()
+  (dolist (key (append yas/next-field-key yas/prev-field-key))
+    (define-key yas/keymap (read-kbd-macro key) nil))
+  (setq yas/next-field-key '("C-/")
+        yas/prev-field-key '("C-?"))
+  (define-key yas/keymap (kbd "C-/") #'yas/next-field)
+  (define-key yas/keymap (kbd "C-?") #'yas/prev-field))
+
+(defun mk/yasnippet-hippie-expand ()
+  (eval-after-load 'hippie-exp
+    '(add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)))
+
+(defun mk/yasnippet-snippets ()
+  (yas/load-directory (concat dot-emacs-dir "snippets")))
+
+(defun mk/setup-yasnippet ()
+  (mk/yasnippet-keys)
+  (mk/yasnippet-hippie-expand)
+  (mk/yasnippet-snippets)
   (setq yas/use-menu 'abbreviate)
-  (add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
-  (yas/load-directory (concat dot-emacs-dir "snippets"))
   (yas/global-mode 1))
+
+(when (require 'yasnippet-bundle nil 'noerror)
+  (mk/setup-yasnippet))
 
 (defun yas/fix-trigger-key ()
   (setq yas/fallback-behavior `(apply ,(local-key-binding (kbd "TAB"))))
