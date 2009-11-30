2001cd865c8702b4536dff577316b706dab17d39
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-30 11:00:46 +0100

    Use `windmove' and the (osx) default frame-switching keys

--- emacs.d/mkhl/base/windows.el
+++ emacs.d/mkhl/base/windows.el
@@ -1,11 +1,8 @@
 
-;; switch windows
-(global-set-key (kbd "M-`") 'switch-to-next-window)
-(global-set-key (kbd "M-~") 'switch-to-previous-window)
+;;; `windmove'
 
-(defun switch-to-next-window (arg)
-  (interactive "p")
-  (other-window arg))
-(defun switch-to-previous-window (arg)
-  (interactive "p")
-  (other-window (- arg)))
+(let ((modifier '(control meta)))
+  (global-set-key (vector `(,@modifier left))  'windmove-left)
+  (global-set-key (vector `(,@modifier right)) 'windmove-right)
+  (global-set-key (vector `(,@modifier up))    'windmove-up)
+  (global-set-key (vector `(,@modifier down))  'windmove-down))
