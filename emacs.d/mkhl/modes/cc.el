3a4bed74e35db939a2ba19190e5f74dd890fb2f8
Martin Kuehl <martin.kuehl@gmail.com>
2009-11-30 10:57:31 +0100

    Cleaner loading of `google-c-style'

--- emacs.d/mkhl/modes/cc.el
+++ emacs.d/mkhl/modes/cc.el
@@ -6,14 +6,18 @@
   (c-toggle-auto-newline 1)
   (c-subword-mode t))
 
+(defun mk/google-c-style ()
+  (when (require 'google-c-style nil 'noerror)
+    (c-add-style "Google" google-c-style)
+    (add-to-list 'c-default-style '(objc-mode . "Google"))))
+
 (defun mk/setup-cc-mode ()
   (setq-default c-basic-offset 4)
   (setq c-default-style '((java-mode . "java")
                           (awk-mode . "awk")
                           (other . "bsd")))
-  (add-hook 'c-mode-common-hook 'mk/c-mode-common-hook)
-  (when (fboundp 'google-set-c-style)
-    (add-hook 'c-mode-common-hook 'google-set-c-style 'append)))
+  (mk/google-c-style)
+  (add-hook 'c-mode-common-hook 'mk/c-mode-common-hook))
 
 (eval-after-load 'cc-mode
   '(mk/setup-cc-mode))
