f272caa09c12924ca8aab9a26f66145fcd3e00ab
Martin Kuehl <martin.kuehl@gmail.com>
2009-12-02 15:21:30 +0100

    Define bindings for more default mac-like commands

--- emacs.d/mkhl/system/hiding.el
+++ emacs.d/mkhl/system/hiding.el
@@ -1,2 +1,8 @@
 
-(global-set-key [(control meta shift h)] #'ns-do-hide-emacs)
+(dolist* ((char . func) '((?h . ns-do-hide-emacs)
+                          (?j . exchange-point-and-mark)
+                          (?k . kill-this-buffer)
+                          (?n . make-frame)
+                          (?q . save-buffers-kill-emacs)
+                          (?w . delete-frame)))
+  (global-set-key (vector (list 'control 'meta 'shift char)) func))
