ba64c66825890b81c1ce8602c59ff4237e0997d5
Martin Kuehl <martin.kuehl@gmail.com>
2009-12-02 15:21:03 +0100

    Don't try to redefine `ns-do-hide-emacs'

--- emacs.d/mkhl/system/hiding.el
+++ emacs.d/mkhl/system/hiding.el
@@ -1,12 +1,2 @@
 
-(unless (fboundp 'ns-do-hide-emacs)
-  (defun ns-do-hide-emacs ()
-    (interactive)
-    (do-applescript (format "\
-tell application \"System Events\"
-    tell application process \"%s\"
-        set visible to false
-    end tell
-end tell" invocation-name))))
-
 (global-set-key [(control meta shift h)] #'ns-do-hide-emacs)
