283c04a19e05bc67fd5dd71ae5a3e01b5bad94be
Martin Kuehl <martin.kuehl@gmail.com>
2009-12-10 16:18:31 +0100

    Restructure and reenable flymake settings
    Use flymake error functions only when flymake is loaded.

--- emacs.d/mkhl/tools/flymake.el
+++ emacs.d/mkhl/tools/flymake.el
@@ -33,6 +33,24 @@
               (remove* mode auto-mode-alist :test-not 'eq :key 'cdr))
       (pushnew (list regexp func) flymake-allowed-file-name-masks))))
 
+;;; `flymake' compatible `next-error' replacements
+
+(defun mk/setup-flymake-error-bindings ()
+  (defun mk/navigate-errors (compile-func flymake-func)
+    (if (condition-case nil (next-error-find-buffer) (error nil))
+        (call-interactively compile-func)
+      (call-interactively flymake-func)
+      (let ((err (get-char-property (point) 'help-echo)))
+        (when err (message err)))))
+  (defun mk/next-error ()
+    (interactive)
+    (mk/navigate-errors 'next-error 'flymake-goto-next-error))
+  (defun mk/previous-error ()
+    (interactive)
+    (mk/navigate-errors 'previous-error 'flymake-goto-prev-error))
+  (global-set-key [remap next-error] #'mk/next-error)
+  (global-set-key [remap previous-error] #'mk/previous-error))
+
 ;;; `flymake' configuration
 
 (defun mk/setup-flymake ()
@@ -40,27 +58,8 @@
   (defun flymake-xml-init ()
     (list "xmllint" (list "--valid" (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))))
   (mk/flymake-allow-modes)
+  (mk/setup-flymake-error-bindings)
   (add-hook 'find-file-hook 'flymake-find-file-hook))
 
 (when (require 'flymake nil 'noerror)
   (mk/setup-flymake))
-
-;;; `flymake' compatible `next-error' replacements
-
-(defun mk/navigate-errors (compile-func flymake-func)
-  (if (condition-case nil (next-error-find-buffer) (error nil))
-      (call-interactively compile-func)
-    (call-interactively flymake-func)
-    (let ((err (get-char-property (point) 'help-echo)))
-      (when err (message err)))))
-
-(defun mk/next-error ()
-  (interactive)
-  (mk/navigate-errors 'next-error 'flymake-goto-next-error))
-
-(defun mk/previous-error ()
-  (interactive)
-  (mk/navigate-errors 'previous-error 'flymake-goto-prev-error))
-
-(global-set-key [remap next-error] #'mk/next-error)
-(global-set-key [remap previous-error] #'mk/previous-error)
