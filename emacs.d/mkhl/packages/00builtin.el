38c73d862f72bfc77fa59aaee08357c8f2fedd3c
Martin Kuehl <martin.kuehl@gmail.com>
2009-12-10 16:15:00 +0100

    Disable `generic-x'

--- emacs.d/mkhl/packages/00builtin.el
+++ emacs.d/mkhl/packages/00builtin.el
@@ -4,8 +4,6 @@
 (require 'autoload)
 ;; And have the useful stuff available
 (require 'assoc)
-;; Make sure built-in packages like `generic-x' can be overridden.
-(require 'generic-x)
 (require 'jka-compr)
 ;; Make sure `Info-additional-directory-list' is available
 (require 'info)
