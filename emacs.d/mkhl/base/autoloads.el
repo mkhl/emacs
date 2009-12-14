b7fd47f06609ee1c4d828407cdcc18b6636e65a9
Martin Kuehl <martin.kuehl@gmail.com>
2009-12-14 16:09:48 +0100

    Make `local-autoload' advice more robust

--- emacs.d/mkhl/base/autoloads.el
+++ emacs.d/mkhl/base/autoloads.el
@@ -1,10 +1,14 @@
 
 (defadvice update-file-autoloads (around generate-local-autoloads activate)
   "Generate autoloads in a file called `autoloads' in FILE's directory."
-  (let* ((dir (file-name-as-directory
-               (file-name-directory
-                (file-truename (ad-get-arg 0)))))
-         (generated-autoload-file (if (null generated-autoload-file)
-                                      (concat dir "autoloads.el")
+  (let* ((local-directory (file-name-as-directory
+                           (file-name-directory
+                            (file-truename (ad-get-arg 0)))))
+         (local-autoload-file (concat local-directory "autoloads.el"))
+         (use-local-autoload-file (or (null generated-autoload-file)
+                                      (not (file-name-absolute-p
+                                            generated-autoload-file))))
+         (generated-autoload-file (if use-local-autoload-file
+                                      local-autoload-file
                                     generated-autoload-file)))
     ad-do-it))
