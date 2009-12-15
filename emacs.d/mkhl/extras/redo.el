5cc27f388a9b97254db24dd4ad53c26b320d4db7
Martin Kuehl <martin.kuehl@gmail.com>
2009-12-15 02:23:59 +0100

    Clean up non-NS bindings

--- emacs.d/mkhl/extras/redo.el
+++ emacs.d/mkhl/extras/redo.el
@@ -2,6 +2,4 @@
 (when (require 'redo nil 'noerror)
   (global-set-key (kbd "M-z") #'undo)
   (global-set-key (kbd "M-Z") #'redo)
-  (global-set-key (kbd "s-z") #'undo)
-  (global-set-key (kbd "s-Z") #'redo)
   (global-set-key (kbd "C-z") #'zap-to-char))
