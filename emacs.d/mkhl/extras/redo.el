65f0db9f53ba1b9005b53ed2331820665e09d98d
Martin Kuehl <martin.kuehl@gmail.com>
2010-01-27 00:08:25 +0100

    `M-S-x' notation doesn't seem to work.
    `C-S-x' works fine, as does `M-X', but not `C-X'.  Strangeness…

--- emacs.d/mkhl/extras/redo.el
+++ emacs.d/mkhl/extras/redo.el
@@ -1,5 +1,5 @@
 
 (when (require 'redo nil 'noerror)
   (global-set-key (kbd "M-z") #'undo)
-  (global-set-key (kbd "M-S-z") #'redo)
+  (global-set-key (kbd "M-Z") #'redo)
   (global-set-key (kbd "C-z") #'zap-to-char))
