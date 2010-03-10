01ce8a5cd29203a11993fe04003e1b79fcc06dda
Martin Kühl <martin.kuehl@gmail.com>
2010-03-11 00:37:15 +0100

    Custom: Adapt the default-face declaration to Carbon Emacsen.
    Carbon-based Emacsen seem to require a different (or at least a lot more
    verbose) declaration for custom-set faces, so adapt the declaration.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -9,4 +9,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :foundry "apple" :family "Inconsolata")))))
+ '(default ((t (:stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :family "apple-inconsolata")))))
