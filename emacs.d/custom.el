dc3cde380b241030970dd19c6a4f4de728bb0a33
Martin Kühl <martin.kuehl@gmail.com>
2010-07-26 23:21:38 +0200

    Emacs: Revert to Menlo.
    Menlo has more variants and symbols and is available by default.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -9,4 +9,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :foundry "apple" :family "Inconsolata")))))
+ '(default ((t (:height 110 :foundry "apple" :family "Menlo")))))
