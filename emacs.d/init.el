2179f0bd293c453ffe96ce01cc2b7e106433ea7d
Martin Kühl <martin.kuehl@gmail.com>
2010-10-04 21:56:27 +0200

    Emacs: Use `user-emacs-directory' instead of a custom variable.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,22 +3,17 @@
 (require 'assoc)
 (setq vc-follow-symlinks t)
 
-;;; `dot-emacs-dir'
-(unless (boundp 'dot-emacs-dir)
-  (let ((this-file (or load-file-name (buffer-file-name))))
-    (setq dot-emacs-dir (file-name-directory this-file))))
-
 ;;; `org' and `org-babel'
 (dolist (path '("org/lisp" "org/contrib/lisp"))
-  (add-to-list 'load-path (expand-file-name path dot-emacs-dir)))
+  (add-to-list 'load-path (expand-file-name path user-emacs-directory)))
 (require 'org-install)
 
 ;;; Load `*.org' using `org-babel'
-(dolist (file (directory-files dot-emacs-dir 'full (rx ".org" eos)))
+(dolist (file (directory-files user-emacs-directory 'full (rx ".org" eos)))
   (org-babel-load-file file))
 
 ;;; `custom-file'
-(setq custom-file (expand-file-name "custom.el" dot-emacs-dir))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror 'nomessage)
 
 ;;; `emacs-init-time'
